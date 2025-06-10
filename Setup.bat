838d53bdab0febc8dbc21457f02bb51360ec579c
Nara <107673865+SyberTedy@users.noreply.github.com>
2025-06-10 17:25:19 +0300

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,6 +1,9 @@
 cd API
 dotnet restore
 cd ../
+cd WebSite
+npm install
+cd ../
 del "%USERPROFILE%\TransferRequestAppDB.mdf"
 del "%USERPROFILE%\TransferRequestAppDB_log.ldf"
 sqlcmd -S (LocalDB)\MSSQLLocalDB -i TransferRequestAppDB.sql
